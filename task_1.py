0d92b0c0b1682960f1d94bc700f45343e084fad9
Daniya <113026432+DaniyaKu@users.noreply.github.com>
2022-09-29 22:40:10 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math
-import numpy
-import matplotlib.pyplot as mpp
+import math # импорт библиотеки (модуля)
+import numpy # импорт библиотеки для создание массива
+import matplotlib.pyplot as mpp # импорт библиотеки для создания графикав
 
 # Эта программа рисует график функции, заданной выражением ниже
 
-if __name__=='__main__':
-    arguments = numpy.arange(0, 200, 0.1)
+if __name__=='__main__': # для того, чтобы обезопасить импорт модуля от нежелательных побочных эффектов
+    arguments = numpy.arange(0, 200, 0.1) # задание промежутков
     mpp.plot(
         arguments,
-        [math.sin(a) * math.sin(a/20.0) for a in arguments]
+        [math.sin(a) * math.sin(a/20.0) for a in arguments] # построение графика
     )
-    mpp.show()
+    mpp.show() # показ графика на экране
